3c555d537f69511a4eaec736f5a5f8093461782d
1402nikhil <nikhiljagdale200414@gmail.com>
2025-05-17 19:58:36 +0530

    Remove build, install, and log directories from tracking

--- controller_ws/src/three_omniwheel_controller/build/three_omniwheel_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nikhil/Work/ROS/controller_ws/src/three_omniwheel_controller
-# Build directory: /home/nikhil/Work/ROS/controller_ws/src/three_omniwheel_controller/build/three_omniwheel_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
